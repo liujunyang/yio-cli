33a39960d8a27242e797ff97ab1e682531e88274
liujunyang <liujunyangscript@gmail.com>
2017-12-22 10:57:57 +0800

    link core

--- bin/index.js
+++ bin/index.js
@@ -13,4 +13,22 @@ const commander = require('commander')
 
 const cacheDir = path.join(process.env.HOME, '.yio/yio-core-module')
 
-console.log(cacheDir)
+require('keep-module-latest')({
+	moduleName: 'yio-core',
+	cwd: cacheDir,
+	registry: 'https://registry.npmjs.org/',
+	beforeInstall: () => {
+		console.log('do beforeInstall')
+		if (fse.existSync(cwd)) {
+			try {
+				fse.removeSync(cwd)
+			} catch (err) {
+				throw new Error(err)
+			}
+		}
+	},
+}).then(modulePath => {
+	console.log(modulePath)
+}).catch(err => {
+	console.error(err)
+})
